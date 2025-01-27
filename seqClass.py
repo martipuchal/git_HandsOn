4e0e372deb98cf6b6748f4f6865c0bfff24b3c48
Marti Puchal <marti.puchal@gmail.com>
2025-01-27 17:12:50 +0100

    branchchange

--- seqClass.py
+++ seqClass.py
@@ -28,6 +28,6 @@ if args.motif:
     args.motif = args.motif.upper()
     print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end = '')
     if re.search(args.motif, args.seq):
-        print("FOUND")
+        print("We FOUND it")
     else:
         print("NOT FOUND")
